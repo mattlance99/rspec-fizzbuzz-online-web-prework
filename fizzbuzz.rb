5a3720756738bb0117ea2395f61ee669ecdc4d7c
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-13 22:39:02 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 != 0  
+    fizz_string = "Fizz"
+    fizz_string
+  elsif int % 3 != 0 && int % 5 == 0 
+    buzz_string = "Buzz"
+    buzz_string
+  elsif int % 3 == 0 && int % 5 == 0 
+    fizzbuzz_string = "FizzBuzz"
+    puts fizzbuzz_string
+    fizzbuzz_string
+ end
+end
